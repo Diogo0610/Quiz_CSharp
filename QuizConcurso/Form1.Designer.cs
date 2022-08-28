96dfe041d81f69cffafe97395961d45304c10930
Diogo Brandon <d_bb2001@hotmail.com>
2022-08-27 22:57:40 -0300

    Questions

--- QuizConcurso/Form1.Designer.cs
+++ QuizConcurso/Form1.Designer.cs
@@ -28,96 +28,57 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.lblQuestion = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.button4 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
-            // lblQuestion
+            // label1
             // 
-            this.lblQuestion.Anchor = System.Windows.Forms.AnchorStyles.Top;
-            this.lblQuestion.Font = new System.Drawing.Font("Microsoft Tai Le", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblQuestion.Location = new System.Drawing.Point(25, 18);
-            this.lblQuestion.Margin = new System.Windows.Forms.Padding(6, 0, 6, 0);
-            this.lblQuestion.Name = "lblQuestion";
-            this.lblQuestion.Size = new System.Drawing.Size(1273, 312);
-            this.lblQuestion.TabIndex = 0;
-            this.lblQuestion.Text = "Question";
-            this.lblQuestion.UseCompatibleTextRendering = true;
-            this.lblQuestion.Click += new System.EventHandler(this.label1_Click);
+            this.label1.Anchor = System.Windows.Forms.AnchorStyles.Top;
+            this.label1.Location = new System.Drawing.Point(329, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(522, 32);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Escolha qual Quiz você quer praticar:";
             // 
             // button1
             // 
             this.button1.Anchor = System.Windows.Forms.AnchorStyles.Left;
-            this.button1.Location = new System.Drawing.Point(114, 396);
-            this.button1.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.button1.Location = new System.Drawing.Point(214, 377);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(348, 144);
+            this.button1.Size = new System.Drawing.Size(268, 63);
             this.button1.TabIndex = 1;
-            this.button1.Tag = "1";
-            this.button1.Text = "button1";
+            this.button1.Text = "Sistemas";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.checkAnswerEvent);
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // button2
             // 
             this.button2.Anchor = System.Windows.Forms.AnchorStyles.Right;
-            this.button2.Location = new System.Drawing.Point(834, 396);
-            this.button2.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
+            this.button2.Location = new System.Drawing.Point(525, 377);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(348, 144);
+            this.button2.Size = new System.Drawing.Size(268, 63);
             this.button2.TabIndex = 2;
-            this.button2.Tag = "2";
-            this.button2.Text = "button2";
+            this.button2.Text = "Suporte";
             this.button2.UseVisualStyleBackColor = true;
-            this.button2.Click += new System.EventHandler(this.checkAnswerEvent);
-            // 
-            // button3
-            // 
-            this.button3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.button3.Location = new System.Drawing.Point(114, 785);
-            this.button3.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(348, 144);
-            this.button3.TabIndex = 3;
-            this.button3.Tag = "3";
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            this.button3.Click += new System.EventHandler(this.checkAnswerEvent);
-            // 
-            // button4
-            // 
-            this.button4.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.button4.Location = new System.Drawing.Point(834, 785);
-            this.button4.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
-            this.button4.Name = "button4";
-            this.button4.Size = new System.Drawing.Size(348, 144);
-            this.button4.TabIndex = 4;
-            this.button4.Tag = "4";
-            this.button4.Text = "button4";
-            this.button4.UseVisualStyleBackColor = true;
-            this.button4.Click += new System.EventHandler(this.checkAnswerEvent);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(17F, 32F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
             this.BackColor = System.Drawing.Color.SteelBlue;
             this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
-            this.ClientSize = new System.Drawing.Size(1325, 1050);
-            this.Controls.Add(this.button4);
-            this.Controls.Add(this.button3);
+            this.ClientSize = new System.Drawing.Size(1002, 664);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
-            this.Controls.Add(this.lblQuestion);
+            this.Controls.Add(this.label1);
             this.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.Margin = new System.Windows.Forms.Padding(6, 5, 6, 5);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Form1";
+            this.Text = "Quiz";
             this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
@@ -126,11 +87,9 @@
 
         #endregion
 
-        private System.Windows.Forms.Label lblQuestion;
+        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.Button button4;
     }
 }
 
---
+++ QuizConcurso/FormSistemas.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QuizConcurso
+{
+    public partial class FormSistemas : Form
+    {
+        int correctAnswer;
+        int questionNumber = 1;
+        public FormSistemas()
+        {
+            InitializeComponent();
+            askQuestion(questionNumber);
+        }
+
+        private void CheckAnswer(object sender, EventArgs e)
+        {
+            var senderObject = (Button)sender;
+
+            int buttonTag = Convert.ToInt32(senderObject.Tag);
+
+            if (buttonTag == correctAnswer)
+            {
+                MessageBox.Show("Você acertou! Clique em 'Ok' para continuar.", "Quiz");
+                questionNumber++;
+                askQuestion(questionNumber);
+            }
+            else
+            {
+                switch (correctAnswer)
+                {
+                    case 1:
+                        MessageBox.Show($"Você errou! A resposta correta é a alternativa '{button1.Text}'." +
+                        " Clique em 'Ok' para continuar.", "Quiz");
+                        questionNumber++;
+                        askQuestion(questionNumber);
+                        break;
+                    case 2:
+                        MessageBox.Show($"Você errou! A resposta correta é a alternativa '{button2.Text}'." +
+                        " Clique em 'Ok' para continuar.", "Quiz");
+                        questionNumber++;
+                        askQuestion(questionNumber);
+                        break;
+                    case 3:
+                        MessageBox.Show($"Você errou! A resposta correta é a alternativa '{button3.Text}'." +
+                        " Clique em 'Ok' para continuar.", "Quiz");
+                        questionNumber++;
+                        askQuestion(questionNumber);
+                        break;
+                    case 4:
+                        MessageBox.Show($"Você errou! A resposta correta é a alternativa '{button4.Text}'." +
+                        " Clique em 'Ok' para continuar.", "Quiz");
+                        questionNumber++;
+                        askQuestion(questionNumber);
+                        break;
+                }
+            }
+        }
+
+        private void askQuestion(int questionNum)
+        {
+            switch (questionNum)
+            {
+                case 1:
+                    lblQuestion.Text = "“Uma declaração do método final nunca pode mudar, assim todas as " +
+                        "subclasses utilizam a mesma implementação do método; e chamadas métodos final são " +
+                        "resolvidas em tempo de compilação – isso é conhecido como _________________.” " +
+                        "Acerca da linguagem Java, assinale a alternativa que completa corretamente a " +
+                        "afirmativa:";
+                    button1.Text = "A) subclasse";
+                    button2.Text = "B) superclasse ";
+                    button3.Text = "C) vinculação estática";
+                    button4.Text = "D) interface de marcação";
+                    correctAnswer = 3;
+                    break;
+                case 2:
+                    lblQuestion.Text = "Sobre a linguagem Java, analise as afirmativas a seguir." + Environment.NewLine +
+                        "I.As instruções de um switch geralmente terminam com uma instrução break que termina a " +
+                        "execução em um while, por exemplo. " + Environment.NewLine +
+                        "II.A instrução break, quando executada em um while, " +
+                        "for, do…while ou switch, ocasiona a saída imediata dessa instrução. " + Environment.NewLine +
+                        "III.Os operadores & e | funcionam de forma idêntica aos operadores && e ||, mas sempre avaliam ambos os " +
+                        "operandos." + Environment.NewLine + "IV.Cada valor que se deseja testar em um scanner deve ser " +
+                        "listado em um rótulo case separado." + Environment.NewLine + "Está(ão) correta(s)apenas a(s) afirmativa(s)";
+                    button1.Text = "A) II";
+                    button2.Text = "B) III";
+                    button3.Text = "C) I e IV";
+                    button4.Text = "D) II e III";
+                    correctAnswer = 4;
+                    break;
+                case 3:
+                    lblQuestion.Text = "O PHP possui diversas funções que são utilizadas para manipulação de imagens. Uma dessas " +
+                        "funções define o modo de transparência de uma imagem. Assinale a alternativa correta que apresenta essa função.";
+                    button1.Text = "A) gd_info";
+                    button2.Text = "B) getimagesize";
+                    button3.Text = "C) imagealphablending";
+                    button4.Text = "D) imagecolorallocatealpha";
+                    correctAnswer = 3;
+                    break;
+                case 4:
+                    lblQuestion.Text = "“Sobre comandos do PHP para diretórios, esta função obtém o diretório corrente de trabalho.”" +
+                        " Trata-se da função:";
+                    button1.Text = "A) dir";
+                    button2.Text = "B) readdir";
+                    button3.Text = "C) getcwd";
+                    button4.Text = "D) opendir";
+                    correctAnswer = 3;
+                    break;
+                case 5:
+                    lblQuestion.Text = "“Acerca dos comandos em PHP, sobre arrays, uma das funções retorna um array com chaves e valores trocados.”" +
+                        " Essa função denomina-se:";
+                    button1.Text = "A) array_fill";
+                    button2.Text = "B) array-flip";
+                    button3.Text = "C) array_keys";
+                    button4.Text = "D) array_merge";
+                    correctAnswer = 2;
+                    break;
+                case 6:
+                    lblQuestion.Text = "Acerca das métricas de confiabilidade, “a confiabilidade pode ser especificada como a probabilidade de uma " +
+                        "falha de sistema ocorrer quando um sistema estiver em uso dentro de um ambiente operacional especificado”. Existem duas " +
+                        "métricas importantes para especificar a confiabilidade e uma adicional para especificar os atributos de disponibilidade, " +
+                        "que estão relacionadas ao sistema.São essas métricas, EXCETO: ";
+                    button1.Text = "A) Disponibilidade";
+                    button2.Text = "B) Taxa de ocorrência de falhas";
+                    button3.Text = "C) Probabilidade de falha sob demanda";
+                    button4.Text = "D) Número de transações entre as falhas";
+                    correctAnswer = 4;
+                    break;
+                case 7:
+                    lblQuestion.Text = "Garantir a qualidade de um software engloba diversos fatores, bem como muitas atividades, " +
+                        "concentradas na gestãoda qualidade do software, em que várias ações são apresentadas. “Uma dessas ações é uma função de " +
+                        "controle dequalidade, mas que tem como objetivo principal descobrir erros”. Assinale a alternativa que apresenta essa ação.";
+                    button1.Text = "A) Testes";
+                    button2.Text = "B) Padrões";
+                    button3.Text = "C) Proteção";
+                    button4.Text = "D) Administração de segurança";
+                    correctAnswer = 1;
+                    break;
+            }
+        }
+    }
+}
